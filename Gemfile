ffa6b8d0b36ae4533ebfc66a9a2a99f85488d839
Chad Metcalf <cm0591@gmail.com>
2015-10-25 13:16:17 -0500

    Background Images

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source "https://rubygems.org"
 
 gem 'jekyll'
-gem 'rack-jekyll'
+gem 'guard-jekyll'
--- _vendor/bundle/ruby/2.1.0/gems/redcarpet-2.3.0/ext/redcarpet/siteconf20131221-43691-84snuj.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131221-43691-1n6xgj8"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
